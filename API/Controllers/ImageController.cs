8172f499f51c2dcfbb4501131a3c3c7504538897
Abdul.i1 <abdul.i1@schule.bremen.com>
2024-11-10 00:07:40 +0100

    implemented ImageController

---
+++ API/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿// ------------------------------------------------------------------
+// © Copyright 2024 Thermo Fisher Scientific Inc. All rights reserved.
+// ------------------------------------------------------------------
+using DateAppApi.Core;
+using DateAppApi.DbContexts;
+using DateAppApi.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateAppApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ImageController : BaseAuthController
+    {
+        public ImageController(IJwtService jwtService, AppDbContext context) : base(jwtService, context)
+        {
+            m_context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var image = await m_context.Images.FindAsync(id);
+            if (image == null) return NotFound();
+            return File(image.Data, "image/jpeg");
+        }
+
+        #region private fields and constants
+        private readonly AppDbContext m_context;
+        #endregion
+    }
+}
