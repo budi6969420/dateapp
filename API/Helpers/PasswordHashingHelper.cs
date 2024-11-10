15fa6c28b349f4d422fadb93b74245dffb253082
Abdul.i1 <abdul.i1@schule.bremen.com>
2024-11-10 01:04:10 +0100

    reimplemented hashing

--- API/Helpers/PasswordHashingHelper.cs
+++ API/Helpers/PasswordHashingHelper.cs
@@ -1,10 +1,30 @@
 ﻿// ------------------------------------------------------------------
 // © Copyright 2024 Thermo Fisher Scientific Inc. All rights reserved.
 // ------------------------------------------------------------------
+using System.Security.Cryptography;
+using System.Text;
+
 namespace DateAppApi.Helpers
 {
     public static class PasswordHashingHelper
     {
-        public static string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convert the input string to a byte array and compute the hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2")); // Convert each byte to a hex value
+                }
+
+                // Return the hexadecimal string
+                return builder.ToString();
+            }
+        }
     }
 }
